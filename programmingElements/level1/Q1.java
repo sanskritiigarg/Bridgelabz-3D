ebe689ba127df54ecb30dea40390f5a4d6391c5d
Sanskriti Garg <sanskritii.garg@gmail.com>
2025-09-17 20:32:29 +0530

    Resubmission after creating OOPS branch

--- programmingElements/level1/Q1.java
+++
@@ -1,9 +0,0 @@
-public class Q1 {
-    public static void main(String[] args) {
-        int birthYear = 2000;
-        int currYear = 2024;
-        
-        int age = currYear - birthYear;
-        System.out.println("Harry's age in 2024 is " + age);
-    }
-}
